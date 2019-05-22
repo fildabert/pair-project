5b75d041ea8ef3af00329454511bedf9a6b45ce5
fildabert <filberttt@hotmail.com>
2019-05-22 16:28:04 +0700

    added ejs

--- controllers/gunController.js
+++ controllers/gunController.js
@@ -2,9 +2,19 @@ const db = require("../models/index")
 
 class gunController{
     static showGuns(req, res){
-        return res.locals.allGuns
+        var smg = db.Smg.findAll()
+        var assaultrifle = db.AssaultRifle.findAll()
+        var handgun = db.Handgun.findAll()
+        var sniper = db.SniperRifle.findAll()
+        return Promise.all([smg, assaultrifle, handgun, sniper])
             .then(allGunz =>{
-                res.send(allGunz)
+                res.render("guns-display.ejs", {
+                    smg: allGunz[0],
+                    assaultrifle: allGunz[1],
+                    handgun: allGunz[2],
+                    sniper: allGunz[3]
+
+                })
             })
     }
 }
